a5826c070ddf6c86bffbbfccb12e32dd25ce92b8
tdalal <89410493+tdalal17@users.noreply.github.com>
2024-04-27 12:04:14 -0500

    made a path problem

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/conferences', conferenceRoutes);
 app.use('/weather', weatherRoutes);
 app.use('/auth', authRoutes);
-app.use('/', indexRoutes);
+app.use('/indexRoutes', indexRoutes);
 app.use('/conferences', conferenceRoutes);
 app.use('/weather', weatherRoutes);
 
