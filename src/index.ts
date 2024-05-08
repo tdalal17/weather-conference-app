e28d911d0e8cdc35cdbe0dadfdee1cae96eb5ad2
tdalal <89410493+tdalal17@users.noreply.github.com>
2024-05-08 15:03:29 -0500

    addeding middlewere for backend AND front end

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import { initializeDatabase } from './database';
 import conferenceRoutes from './conferenceRoutes';
 import weatherRoutes from './weatherRoutes';
@@ -7,18 +8,23 @@ import authRoutes from './authRoutes';
 import indexRoutes from './indexRoutes';
 import 'reflect-metadata';
 
+
 const app = express();
 const port = 8080;
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'conference-app', 'build')));
+
 app.use('/', indexRoutes);
 app.use('/conferences', conferenceRoutes);
 app.use('/weather', weatherRoutes);
 app.use('/auth', authRoutes);
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'conference-app', 'build', 'index.html'));
+});
 
 app.listen(port, async () => {
   await initializeDatabase();
